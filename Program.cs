f245f1d079bbead66d5488e8f6d0a3acf5cfccc5
Maher Husein <maherle03@gmail.com>
2020-03-19 23:05:05 -0400

    intialized the project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DotNetTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
