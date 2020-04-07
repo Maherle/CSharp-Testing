5f4f36aa61f227913d68bb09d7819a931db1f144
Maher Husein <maherle03@gmail.com>
2020-04-06 21:52:59 -0400

    testing structs and classes

--- Program.cs
+++ Program.cs
@@ -6,7 +6,13 @@ namespace DotNetTesting
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var defaultClassTest = new  classes.ClassTest();
+            var paramClasstest = new classes.ClassTest(10,"Params");
+            var structTest = new classes.StructTest(3, "Struct");
+
+            Console.WriteLine(defaultClassTest.ToString());
+            Console.WriteLine(paramClasstest.ToString());
+            Console.WriteLine(structTest.ToString());
         }
     }
 }
